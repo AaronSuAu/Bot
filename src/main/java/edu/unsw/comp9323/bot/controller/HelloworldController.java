c69500b3bef7490bf38ee7598942cc37e5f6c9f5
Char1ei <zlinaus@gmail.com>
2017-09-05 22:10:06 +1000

    google cloud version setup

--- src/main/java/edu/unsw/comp9323/bot/controller/HelloworldController.java
+++ src/main/java/edu/unsw/comp9323/bot/controller/HelloworldController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloworldController {
 	@GetMapping("/")
 	public String hello() {
-		return "Hello World";
+		return "👾";
 	}
 }
