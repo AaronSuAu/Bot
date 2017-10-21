a1b1b238d144e4db7d467f2e69c90dc0c1238189
YIJUN <1015585546@qq.com>
2017-10-21 17:10:10 +1100

    fix email & debug

--- src/main/java/edu/unsw/comp9323/bot/constant/Constant.java
+++ src/main/java/edu/unsw/comp9323/bot/constant/Constant.java
@@ -7,4 +7,6 @@ public class Constant {
 	public final static Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$",
 			Pattern.CASE_INSENSITIVE);
 	public final static String DOMAIN_NAME = "https://cmbot-b3f5e.appspot.com";
+	public final static String EMAIL_NAME = "comp9323bot@gmail.com";
+	public final static String EMAIL_PASS = "comp9323";
 }
